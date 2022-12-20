89345ca813322c23e4f9ec2c7216271016142ac5
Antonio Thiago <ladeiragrande777@gmail.com>
2022-12-20 19:24:44 -0300

    active menu settings

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 export const home = (req: Request, res: Response) => {
     res.render('pages/page', {
         banner: {
+            menu: '/all',
             title: 'Todos os animais',
             background: 'allanimals.jpg'
         }
@@ -11,6 +12,7 @@ export const home = (req: Request, res: Response) => {
 export const dogs = (req: Request, res: Response) => {
     res.render('pages/page', {
         banner: {
+            menu: '/dogs',
             title: 'Cachorros',
             background: 'banner_dog.jpg'
         }
@@ -19,6 +21,7 @@ export const dogs = (req: Request, res: Response) => {
 export const cats = (req: Request, res: Response) => {
     res.render('pages/page',{
         banner: {
+            menu: '/cats',
             title: 'Gatos',
             background: 'banner_cat.jpg'
         }
@@ -27,6 +30,7 @@ export const cats = (req: Request, res: Response) => {
 export const fishes = (req: Request, res: Response) => {
     res.render('pages/page', {
         banner: {
+            menu: '/fishes',
             title: 'Peixes',
             background: 'banner_fish.jpg'
         }
